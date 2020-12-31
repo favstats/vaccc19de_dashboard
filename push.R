81b267c8ff80215c67f243423e232038266af3ec
favstats <fabio.votta@gmail.com>
2020-12-31 18:57:29 +0100

    2020-12-31 18:57:29: Update Dashboard

--- push.R
+++ push.R
@@ -1,13 +1,13 @@
 
 
 while(T){
-  unlink("docs")
+  unlink("docs", recursive = T, force = T)
 
   rmarkdown::render_site()
 
-  Sys.sleep(1)
+  R.utils::copyDirectory("_site/", "docs")
 
-  system("mv _site docs")
+  unlink("_site", recursive = T, force = T)
 
   system("git add -A")
   system(glue::glue('git commit -m "{Sys.time()}: Update Dashboard"'))
