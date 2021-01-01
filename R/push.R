e31ee3f299985e732d562048199ba090aa3caeaf
favstats <fabio.votta@gmail.com>
2021-01-01 16:47:02 +0100

    2021-01-01 16:47:02: Update Dashboard

---
+++ R/push.R
@@ -0,0 +1,21 @@
+
+# Sys.sleep(60*60*18)
+
+while(T){
+  unlink("docs", recursive = T, force = T)
+  unlink("site/_site", recursive = T, force = T)
+
+  rmarkdown::render_site()
+
+  R.utils::copyDirectory("site/_site/", "docs")
+
+  unlink("_site", recursive = T, force = T)
+
+  system("git add -A")
+  system(glue::glue('git commit -m "{Sys.time()}: Update Dashboard"'))
+  system("git push")
+
+  Sys.sleep(60*60*24)
+}
+
+
