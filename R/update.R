2818c8f313b51725d1349e1fe274fbd282cb3b07
favstats <fabio.votta@gmail.com>
2021-01-01 18:09:12 +0100

    try again

--- R/update.R
+++ R/update.R
@@ -1,15 +1,18 @@
 
 # Sys.sleep(60*60*18)
 
+print(getwd())
+rmarkdown::render("site/test.Rmd", output_file = "test.html")
+
 # while(T){
-  unlink("docs", recursive = T, force = T)
-  unlink("site/_site", recursive = T, force = T)
-
-  rmarkdown::render_site("site")
-  rmarkdown::render("README.Rmd")
-
-  dir.create("docs")
-  R.utils::copyDirectory("site/_site/", "docs")
+  # unlink("docs", recursive = T, force = T)
+  # unlink("site/_site", recursive = T, force = T)
+  #
+  # rmarkdown::render_site("site")
+  # rmarkdown::render("README.Rmd")
+  #
+  # dir.create("docs")
+  # R.utils::copyDirectory("site/_site/", "docs")
 
   # system("git add -A")
   # system(glue::glue('git commit -m "{Sys.time()}: Update Dashboard"'))
