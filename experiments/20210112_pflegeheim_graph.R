2b1374315aa1679f1897d6012842a9879ef12a24
SylviRz <52199097+SylviRz@users.noreply.github.com>
2021-01-12 22:39:01 +0100

    created share variable and plotted it

--- experiments/20210112_pflegeheim_graph.R
+++ experiments/20210112_pflegeheim_graph.R
@@ -17,33 +17,35 @@ summary(cumulative_ts$ts_datenstand)
 #loading the Pflegeheim-Data from Regionalstatistik (Table 22411-01-01-4-B)
 
 
-availability<-read_delim("/Users/rzepka/Documents/Correlaid/contribution_for_vaccc19/22411-01-01-4-B.csv",
+availability<-read_delim("/Users/rzepka/Documents/Correlaid/vaccc19de_dashboard/experiments/22411-01-01-4-B.csv",
                        delim=";", skip = 4, locale = locale(encoding = "latin1"))
 
 
 availability_clean<-availability %>%
   janitor::clean_names(dat = ., case = "snake")%>%
   # Select all but last 4 rows (which don't contain information)
-  head(n = -4)%>% 
+  head(n = -4)%>%
   select(x3,stationar_1)%>%
   rename("bundesland"="x3",
-         "Stationäre Plätze"="stationar_1") # verfügbar in Pflegeheimen
+         "platz"="stationar_1") # Stationäre_Plätze verfügbar in Pflegeheimen
 
-availability_clean<-availability_clean[3:18,]        
+# Drop rows without info
+availability_clean<-availability_clean[3:18,]
 
-  
-  
+# Rename Baden-Württemberg
+availability_clean[8,1]<-"Baden-Württemberg"
 
+# Nur aktuellsten Datenstand behalten
 cumulative_ts_aktuell<- cumulative_ts %>%
   # nur aktuellen Tag behalten
-  filter(ts_datenstand==max(ts_datenstand)) %>%
-  arrange(desc(pflegeheim_bewohner_in)) %>%
+  filter(ts_datenstand==max(ts_datenstand))%>%
   #joining the 2017 Platzverfügbarkeit
   right_join(availability_clean) %>%
-  mutate()
+  dplyr::mutate(share_pflegeheim=pflegeheim_bewohner_in/as.numeric(platz)*100)
 
-pflegeheim_share<-ggplot(cumulative_ts_aktuell, aes(x=reorder(bundesland_iso, pflegeheim_bewohner_in), y=pflegeheim_bewohner_in)) +
+pflegeheim_share<-ggplot(cumulative_ts_aktuell, aes(x=reorder(bundesland_iso, share_pflegeheim), y=share_pflegeheim)) +
   geom_bar(stat="identity") +
-  labs(y="Pflegeheimbewohner/in", x="Bundesland") +
+  labs(y="Anteil geimpfter, stationärer \nPflegeheimbewohner/innen (%)", x="Bundesland",
+       caption="Daten zu den Pflegeheimen sind der Tabelle 22411-01-01-4-B der Regionalstatistik \nentnommen. Sie geben die Zahl der verfügbaren stationären Plätze in Pflegeheimen wieder.") +
   theme_bw()
 pflegeheim_share
