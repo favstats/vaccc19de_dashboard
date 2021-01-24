62377c4b5e6d0b7f7052cf623853b9ab5c0e938c
Frie <fr1e@pm.me>
2021-01-24 21:58:26 +0100

    remove pipe dependency, max instead of unique

--- R/download_and_update.R
+++ R/download_and_update.R
@@ -10,7 +10,7 @@ rki_data <- read_csv("https://raw.githubusercontent.com/ard-data/2020-rki-impf-a
 historic <- readr::read_csv("data/ard_data.csv")
 
 # check whether the new Datenstand is already in the time series data
-if (unique(rki_data$publication_date) == max(historic$publication_date)) {
+if (max(rki_data$publication_date) == max(historic$publication_date)) {
   # no new data
   print(glue::glue("No new data. Skipping update."))
   readr::write_lines("no_update", "/tmp/ts_datenstand.txt")
@@ -19,8 +19,8 @@ if (unique(rki_data$publication_date) == max(historic$publication_date)) {
   quit(status = 0, save = "no")
 }
 
-rki_data %>% readr::write_csv("data/ard_data.csv")
+readr::write_csv(rki_data, "data/ard_data.csv")
 
 # write out ts_download and ts_datenstand for gh actions
-readr::write_lines(format(unique(rki_data$publication_date), "%Y-%m-%dT%H%M%S", tz = "Europe/Berlin"), "/tmp/ts_datenstand.txt")
+readr::write_lines(format(max(rki_data$publication_date), "%Y-%m-%dT%H%M%S", tz = "Europe/Berlin"), "/tmp/ts_datenstand.txt")
 readr::write_lines(format(ts_download, "%Y-%m-%dT%H%M%S", tz = "Europe/Berlin"), "/tmp/ts_download.txt")
